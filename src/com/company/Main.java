faa0bde7a72c7b4ea132099f0570b112084d97b9
nedzlam <nedzlamemic@icloud.com>
2019-11-19 20:34:19 +0100

    -iskljucena nula

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,10 +10,19 @@ public class Main {
         x = unos.nextInt();
         y = unos.nextInt();
 
+
         com.company.Calculator kalk = new com.company.Calculator();
         System.out.println("Zbir: " +kalk.add(x,y));
         System.out.println("Razlika: " +kalk.sub(x,y));
         System.out.println("Proizvod: " +kalk.mul(x,y));
+        while (x == 0) {
+            System.out.println("Unesite neki drugi broj!");
+            x = unos.nextInt();
+        }
+        while (y == 0) {
+            System.out.println("Unesite neki drugi broj!");
+            y = unos.nextInt();
+        }
         System.out.println("Kolicnik " +kalk.div(x,y));
         System.out.println("Modul " +kalk.mod(x,y));
     }
